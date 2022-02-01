1570fed482910067ce9dfa91cc68bcab3d9f8318
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-01 18:22:25 +0200

    add getAllJogging validation

--- src/routers/jogging.router.ts
+++ src/routers/jogging.router.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import validatorMiddleware from "../middlewares/validator.middleware";
 import * as joggingController from "../controllers/jogging.controller";
 import authMiddleware from "../middlewares/auth.middleware";
-import { createJoggingSchema, updateJoggingSchema } from "../schemas/jogging.schema";
+import { createJoggingSchema, getAllJoggingSchema, updateJoggingSchema } from "../schemas/jogging.schema";
 
 const router = Router();
 
 router.post("/jogging", authMiddleware, validatorMiddleware(createJoggingSchema), joggingController.createJogging);
-router.get("/jogging", authMiddleware, joggingController.getAllJogging);
+router.get("/jogging", authMiddleware, validatorMiddleware(getAllJoggingSchema), joggingController.getAllJogging);
 router.get("/jogging/:id", authMiddleware, joggingController.getJoggingById);
 router.get("/jogging/report", authMiddleware, joggingController.getWeeklyReport);
 router.put("/jogging/:id", authMiddleware, validatorMiddleware(updateJoggingSchema), joggingController.updateJogging);
