40c73a33e2d397fcfbefee4357de08e98a3da95e
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-01 12:33:10 +0200

    add jwt configs

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 export default {
   MONGO_URI: process.env.MONGO_URI as string,
   APP_PORT: parseInt(process.env.APP_PORT as string),
+  ACCESS_TOKEN_TTL: process.env.ACCESS_TOKEN_TTL as string,
+  ACCESS_TOKEN_SECRET_KEY: process.env.ACCESS_TOKEN_SECRET_KEY as string,
 };
