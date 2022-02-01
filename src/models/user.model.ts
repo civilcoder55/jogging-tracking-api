086348fb2d734a3c5c303039e408b623c5278196
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-01 12:28:52 +0200

    add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import { userDocument } from "../interfaces/user.interface";
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+UserSchema.pre("save", async function (next) {
+  // only hash the password if it has been modified (or is new)
+  if (this.isModified("password")) {
+    this.password = bcrypt.hashSync(this.password, 10);
+  }
+  next();
+});
+
+UserSchema.methods.toJSON = function () {
+  const user = this.toObject();
+  delete user.password;
+  delete user.__v;
+  return user;
+};
+
+UserSchema.methods.validatePassword = async function (inputPassword: string) {
+  return bcrypt.compareSync(inputPassword, this.password);
+};
+
+export default mongoose.model<userDocument>("User", UserSchema);
