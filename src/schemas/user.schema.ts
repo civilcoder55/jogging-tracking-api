70a78b0be206b4fb1cacbd76aa2113b57291e6bc
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-01 12:29:43 +0200

    add user validation schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,19 @@
+import { object, string } from "yup";
+
+export const createUserSchema = object({
+  body: object({
+    passwordConfirmation: string()
+      .required("passwordConfirmation field is required")
+      .test("passwords-match", "passwords and passwordConfirmation must match", function (value) {
+        return this.parent.password === value;
+      }),
+    password: string()
+      .required("password field is required")
+      .matches(
+        /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})/,
+        "Password must contain 8 characters, at least one uppercase, one lowercase, one number and one special character"
+      ),
+    email: string().email("email field must be valid email").required("email field is required"),
+    name: string().required("name field is required"),
+  }),
+});
