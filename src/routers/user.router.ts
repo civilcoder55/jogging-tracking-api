7771e571724d85803bdc841f6d0eb8f1d99e6abc
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-01 22:51:35 +0200

    add roles to user endpoints

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,12 +1,34 @@
 import { Router } from "express";
-import { createUserSchema } from "../schemas/user.schema";
+import { createUserSchema, updateUserSchema } from "../schemas/user.schema";
 import validatorMiddleware from "../middlewares/validator.middleware";
 import * as userController from "../controllers/user.controller";
 import authMiddleware from "../middlewares/auth.middleware";
+import guardMiddleware from "../middlewares/guard.middleware";
+import { ROLES } from "../interfaces/role.interface";
 
 const router = Router();
 
-router.post("/user", validatorMiddleware(createUserSchema), userController.createUser);
-router.get("/user", authMiddleware, userController.getUser);
+router.post(
+  "/users",
+  authMiddleware,
+  guardMiddleware([ROLES.MANAGER, ROLES.ADMIN]),
+  validatorMiddleware(createUserSchema),
+  userController.createUser
+);
+router.get("/users", authMiddleware, guardMiddleware([ROLES.MANAGER, ROLES.ADMIN]), userController.getAllUsers);
+router.get(
+  "/users/:id",
+  authMiddleware,
+  guardMiddleware([ROLES.USER, ROLES.MANAGER, ROLES.ADMIN]),
+  userController.getUser
+);
+router.patch(
+  "/users/:id",
+  authMiddleware,
+  validatorMiddleware(updateUserSchema),
+  guardMiddleware([ROLES.USER, ROLES.MANAGER, ROLES.ADMIN]),
+  userController.updateUser
+);
+router.delete("/users/:id", authMiddleware, guardMiddleware([ROLES.MANAGER, ROLES.ADMIN]), userController.deleteUser);
 
 export default router;
