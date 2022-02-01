d5937369e17a10fea653a14057d4b1042ecd32db
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-01 20:59:51 +0200

    add roles to user interface

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import { ROLES } from "./role.interface";
 
 export interface userDocument extends mongoose.Document {
   email: string;
   name: string;
   password: string;
+  role: ROLES;
   createdAt: Date;
   updatedAt: Date;
   validatePassword(inputPassword: string): Promise<boolean>;
