dcb73b30c561308259c2c0eb804f64eff4a520c5
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-01 22:52:26 +0200

    register endpoint

--- src/routers/auth.router.ts
+++ src/routers/auth.router.ts
@@ -3,9 +3,11 @@ import validatorMiddleware from "../middlewares/validator.middleware";
 import * as authController from "../controllers/auth.controller";
 import authMiddleware from "../middlewares/auth.middleware";
 import { loginSchema } from "../schemas/auth.schema";
+import { createUserSchema } from "../schemas/user.schema";
 
 const router = Router();
 
+router.post("/auth/register", validatorMiddleware(createUserSchema), authController.register);
 router.post("/auth/login", validatorMiddleware(loginSchema), authController.login);
 router.post("/auth/logout", authMiddleware, authController.logout);
 
