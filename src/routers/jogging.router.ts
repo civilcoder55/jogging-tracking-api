ccc95864857fad4d0e106c7d7e343b1c0eb00987
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-01 17:37:33 +0200

    add jogging routes

---
+++ src/routers/jogging.router.ts
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import validatorMiddleware from "../middlewares/validator.middleware";
+import * as joggingController from "../controllers/jogging.controller";
+import authMiddleware from "../middlewares/auth.middleware";
+import { createJoggingSchema, updateJoggingSchema } from "../schemas/jogging.schema";
+
+const router = Router();
+
+router.post("/jogging", authMiddleware, validatorMiddleware(createJoggingSchema), joggingController.createJogging);
+router.get("/jogging", authMiddleware, joggingController.getAllJogging);
+router.get("/jogging/:id", authMiddleware, joggingController.getJoggingById);
+router.get("/jogging/report", authMiddleware, joggingController.getWeeklyReport);
+router.put("/jogging/:id", authMiddleware, validatorMiddleware(updateJoggingSchema), joggingController.updateJogging);
+router.delete("/jogging/:id", authMiddleware, joggingController.deleteJogging);
+
+export default router;
