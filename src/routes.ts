f37a4cca10a3f2fb7e5bdda7fff22ec255725344
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-01 12:34:03 +0200

    register all routers in global routes file

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-
+import authRouter from "./routers/auth.router";
+import userRouter from "./routers/user.router";
 
 const router = Router();
 
 /**
  * Register all routes into one Router object
  */
-
+router.use(userRouter);
+router.use(authRouter);
 
 export default router;
