fdfbbb9100ea3ae31ef102f5c2de19c9eb3fc385
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-02 16:31:14 +0200

    register report router

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import joggingRouter from "./routers/jogging.router";
 import adminJoggingRouter from "./routers/admin/jogging.router";
 import profileRouter from "./routers/profile.router";
 import userRouter from "./routers/user.router";
+import reportRouter from "./routers/report.router";
 
 const router = Router();
 
@@ -15,5 +16,6 @@ router.use(authRouter);
 router.use(profileRouter);
 router.use(joggingRouter);
 router.use(adminJoggingRouter);
+router.use(reportRouter)
 
 export default router;
