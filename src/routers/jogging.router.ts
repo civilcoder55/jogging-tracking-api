22e5b3a3b832d9c01df6919751ac5d4340198366
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-02 02:00:30 +0200

    sperate user jogging routes from admin

--- src/routers/jogging.router.ts
+++ src/routers/jogging.router.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
-import validatorMiddleware from "../middlewares/validator.middleware";
-import * as joggingController from "../controllers/jogging.controller";
-import authMiddleware from "../middlewares/auth.middleware";
 import { createJoggingSchema, getAllJoggingSchema, updateJoggingSchema } from "../schemas/jogging.schema";
+import validatorMiddleware from "../middlewares/validator.middleware";
+import authMiddleware from "../middlewares/auth.middleware";
+import * as joggingController from "../controllers/jogging.controller";
 
 const router = Router();
 
 router.post("/jogging", authMiddleware, validatorMiddleware(createJoggingSchema), joggingController.createJogging);
-router.get("/jogging", authMiddleware, validatorMiddleware(getAllJoggingSchema), joggingController.getAllJogging);
+router.get("/jogging", authMiddleware, validatorMiddleware(getAllJoggingSchema), joggingController.getUserJogging);
 router.get("/jogging/:id", authMiddleware, joggingController.getJogging);
-router.get("/jogging/report", authMiddleware, joggingController.getWeeklyReport);
 router.patch("/jogging/:id", authMiddleware, validatorMiddleware(updateJoggingSchema), joggingController.updateJogging);
 router.delete("/jogging/:id", authMiddleware, joggingController.deleteJogging);
 
