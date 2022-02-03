4d87c9add1e7d335d2e7e2c5cbe36d4c8e4f67e2
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-03 07:06:41 +0200

    fix field types

--- src/schemas/jogging.schema.ts
+++ src/schemas/jogging.schema.ts
@@ -3,7 +3,7 @@ import { date, number, object, string } from "yup";
 export const createJoggingSchema = object({
   body: object({
     date: date().typeError("date field must be valid date"),
-    duration: string()
+    duration: number()
       .typeError("duration field must be number")
       .min(1, "duration must be greater or equal 1 second")
       .required("duration field is required"),
@@ -18,7 +18,7 @@ export const createJoggingSchema = object({
 export const updateJoggingSchema = object({
   body: object({
     date: date().typeError("date field must be valid date"),
-    duration: string().typeError("duration field must be number").min(1, "duration must be greater or equal 1 second"),
+    duration: number().typeError("duration field must be number").min(1, "duration must be greater or equal 1 second"),
     distance: number().typeError("distance field must be number").min(1, "distance must be greater or equal 1 meter"),
     name: string().typeError("name field must be string"),
   }),
