4095a0cfb885a5c22518d10e7d59d489f02c6b72
backend_developer <58332033+civilcoder55@users.noreply.github.com>
2022-02-03 17:00:01 +0200

    register swagger routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import adminJoggingRouter from "./routers/admin/jogging.router";
 import profileRouter from "./routers/profile.router";
 import userRouter from "./routers/user.router";
 import reportRouter from "./routers/report.router";
+import swaggerRouter from "./routers/swagger.router";
 
 const router = Router();
 
@@ -16,6 +17,7 @@ router.use(authRouter);
 router.use(profileRouter);
 router.use(joggingRouter);
 router.use(adminJoggingRouter);
-router.use(reportRouter)
+router.use(reportRouter);
+router.use(swaggerRouter);
 
 export default router;
